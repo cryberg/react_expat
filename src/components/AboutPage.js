01fe7c485ecef96235c8fba2fa66ef5d54ca63d6
Claire Ryberg <claire.ryberg@accenture.com>
2018-04-26 13:00:55 +0200

    Update AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -9,8 +9,8 @@ const AboutPage = () => {
 		</p>
 		<h3>See the code:</h3>
 		<ul>
+			<li><a href="https://bitbucket.org/claire-ryberg/react-expat" target="_blank" rel="noopener noreferrer">Bitbucket</a></li>
 			<li><a href="https://github.com/cryberg/react_expat" target="_blank" rel="noopener noreferrer">Github</a></li>
-			<li>Bitbucket link here</li>
 			<li>Api Used: <a href="https://www.apixu.com/api.aspx" target="_blank" rel="noopener noreferrer">APIUX Weather</a></li>
 		</ul>
 		<h3>Improvements To-do list:</h3>
@@ -19,14 +19,15 @@ const AboutPage = () => {
 			<li>Improve the unit test coverage</li>
 			<li>Use the GoogleMaps API to automatically load the weather at your current location</li>
 			<li>Include the local time for each location</li>
-			<li>Allow the user to get the hourly forecast by clicking on a saved location</li>
+			<li>Allow the user to redirect to a paeg with the hourly forecast by clicking on a saved location</li>
 			<li>Fix the content showing before the CSS is loaded</li>
+			<li>Implement dropdown suggestion while typing a location name</li>
 		</ul>
 
 		<h3>Things I&apos;d do differently:</h3>
 		<ul>
 			<li>Pick a different Weather API. This one, at the free level, does not allow for batch calls</li>
-			<li>Rethink the structure for the store</li>
+			<li>Rethink the structure for the store, especially in relation to localStorage</li>
 		</ul>
 	</div>
 	);
